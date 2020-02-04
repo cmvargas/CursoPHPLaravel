c1552247c7b1aceb6923b4de48929f2042c28225
cmvargas <cmvargase@hotmail.com>
2020-02-03 22:40:43 -0500

    REalizando formulario para crear exponse reports

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -2,15 +2,27 @@
 @section('content')
 <div class="row">
     <div class="col">
-        <h1>Reports</h1>
-        <table class="table">
-            @foreach ($expenseReports as $expenseReport)
-            <tr>
-                <td>{{$expenseReport->title}}</td>
-            </tr>
-            @endforeach
-        </table>
-        
+      <div class="row">
+        <div class="col">
+          <h1>New Reports</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+            <a class="btn btn-primary" href="/expense_reports/create">New create a report</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <table class="table">
+              @foreach ($expenseReports as $expenseReport)
+              <tr>
+                  <td>{{$expenseReport->title}}</td>
+              </tr>
+              @endforeach
+          </table>
+        </div>
+      </div>
     </div>
 </div>
 @endsection
